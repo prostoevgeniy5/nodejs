e4e3c893ae84261245c42295614d31abd9e09f2a
prostoevgeniy5 <yevgeniykyr5@ukr.net>
2021-11-07 20:46:53 +0200

    Refactoring. Moved functions to  file function.js

--- app.js
+++ app.js
@@ -6,42 +6,7 @@ console.log('A','A'.charCodeAt(0), 'Z', 'Z'.charCodeAt(0), 'a', 'a'.charCodeAt(0
 
 
 
-encodeCeasar('sjsffgjkhefgj');function encodeCeasar(text) {
-    let temp = [...text];
-    console.log(temp);
-    temp = temp.map((el, ind) => {
-        if(el === 'Z') {
-            return 'A';
-        } else if (el === 'z') {
-            return 'a';
-        } else if (65 <= el.charCodeAt(0) && el.charCodeAt(0) < 90) {
-            return String.fromCharCode(el.charCodeAt(0) + 1)
-        } else if (97 <= el.charCodeAt(0) && el.charCodeAt(0) < 122) {
-            return String.fromCharCode(el.charCodeAt(0) + 1)
-        } else {
-            return el;
-        }
-    })
-    console.log(temp.join(''));
-    return temp.join('')
-}
+let str = encodeCeasar('sjsffgjkhefgj');
+let str2 = decodeCeasar(str);
 
-function decodeCeasar(text) {
-    let temp = [...text];
-    console.log(temp);
-    temp = temp.map((el, ind) => {
-        if(el === 'A') {
-            return 'Z';
-        } else if (el === 'a') {
-            return 'z';
-        } else if (65 < el.charCodeAt(0) && el.charCodeAt(0) <= 90) {
-            return String.fromCharCode(el.charCodeAt(0) - 1)
-        } else if (97 < el.charCodeAt(0) && el.charCodeAt(0) <= 122) {
-            return String.fromCharCode(el.charCodeAt(0) - 1)
-        } else {
-            return el;
-        }
-    })
-    console.log(temp.join(''));
-    return temp.join('')
-}
+console.log(str, '      ', str2)
