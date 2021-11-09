c5aeaf1ac28ae721493b84f54c9d9f712ba10dee
prostoevgeniy5 <yevgeniykyr5@ukr.net>
2021-11-09 15:41:20 +0200

    Created functions of buisnes logica

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const alphabet = require('./data');
-const {encodeCeasar, decodeCeasar} = require('./functions');
+const {encodeCeasar, decodeCeasar, encodeROT8, decodeROT8, encodeDecodeAtbash} = require('./functions');
+const fs = require('fs');
+const process = require('process');
 
-console.log(alphabet)
-console.log('A','A'.charCodeAt(0), 'Z', 'Z'.charCodeAt(0), 'a', 'a'.charCodeAt(0), 'z', 'z'.charCodeAt(0), 'probel', ' '.charCodeAt(0), '_', '_'.charCodeAt(0));
+let str = '';
+console.log(process.argv)
+
+const readStream = fs.createReadStream('./solution/input.txt');
+const writeStream = fs.createWriteStream('./solution/output.txt');
+
+readStream.on('data', (chunk) => {
+    str += decodeROT8(chunk.toString());
+    writeStream.write(str);
+})
+
+readStream.on('error', (error) => {
+    console.log(error.stack);
+    writeStream.write(`Result with error ${error.stack}`)
+})
+// console.log(alphabet)
+// console.log('A','A'.charCodeAt(0), 'Z', 'Z'.charCodeAt(0), 'a', 'a'.charCodeAt(0), 'z', 'z'.charCodeAt(0), 'probel', ' '.charCodeAt(0), '_', '_'.charCodeAt(0));
 
 
 
-let str = encodeCeasar('sjsffgjkhefgj');
-let str2 = decodeCeasar(str);
+// let str = encodeCeasar('sjsffgjkhefgj');
+// let str2 = decodeCeasar(str);
 
-console.log(str, '      ', str2)
+// console.log(str, '      ', str2)
--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 function encodeCeasar(text) {
     let temp = [...text];
-    console.log(temp);
+    // console.log(temp);
     temp = temp.map((el, ind) => {
         if(el === 'Z') {
             return 'A';
@@ -14,13 +14,14 @@ function encodeCeasar(text) {
             return el;
         }
     })
+    console.log(text)
     console.log(temp.join(''));
     return temp.join('')
 }
 
 function decodeCeasar(text) {
     let temp = [...text];
-    console.log(temp);
+    // console.log(temp);
     temp = temp.map((el, ind) => {
         if(el === 'A') {
             return 'Z';
@@ -34,8 +35,71 @@ function decodeCeasar(text) {
             return el;
         }
     })
+    console.log(text);
     console.log(temp.join(''));
     return temp.join('')
 }
 
-module.exports = {encodeCeasar, decodeCeasar};
+function encodeDecodeAtbash(text) {
+    let temp = [...text];
+    temp = temp.map((el, ind) => {
+        if (65 <= el.charCodeAt(0) && el.charCodeAt(0) <= 90) {
+            return String.fromCharCode(90 - el.charCodeAt(0) + 65)
+        } else if (97 <= el.charCodeAt(0) && el.charCodeAt(0) <= 122) {
+            return String.fromCharCode(122 - el.charCodeAt(0) + 97)
+        } else {
+            return el;
+        }
+    })
+    console.log(text);
+    console.log(temp.join(''));
+    return temp.join('');
+}
+
+function encodeROT8(text) {
+    let temp = [...text];
+    temp = temp.map((el, ind) => {
+        if (65 <= el.charCodeAt(0) && el.charCodeAt(0) <= 90) {
+            if(el.charCodeAt(0) <= 82) {
+                return String.fromCharCode(el.charCodeAt(0) + 8)
+            } else {
+                return String.fromCharCode(8 - (90 - el.charCodeAt(0)) + 64)
+            }
+        } else if (97 <= el.charCodeAt(0) && el.charCodeAt(0) <= 122) {
+            if(el.charCodeAt(0) <= 114) {
+                return String.fromCharCode(el.charCodeAt(0) + 8)
+            } else {
+                return String.fromCharCode(8 - (122 - el.charCodeAt(0)) + 96)
+            }
+        }
+        return el;
+    })
+    console.log(text);
+    console.log(temp.join(''));
+    return temp.join('');
+}
+
+function decodeROT8(text) {
+    let temp = [...text];
+    temp = temp.map((el, ind) => {
+        if (65 <= el.charCodeAt(0) && el.charCodeAt(0) <= 90) {
+            if(el.charCodeAt(0) >= 73) {
+                return String.fromCharCode(el.charCodeAt(0) - 8)
+            } else {
+                return String.fromCharCode(90 - ( 7 - el.charCodeAt(0) + 65))
+            }
+        } else if (97 <= el.charCodeAt(0) && el.charCodeAt(0) <= 122) {
+            if(el.charCodeAt(0) >= 105) {
+                return String.fromCharCode(el.charCodeAt(0) - 8)
+            } else {
+                return String.fromCharCode(122 - ( 7 - el.charCodeAt(0) + 97))
+            }
+        }
+        return el;
+    })
+    console.log(text);
+    console.log(temp.join(''));
+    return temp.join('');
+}
+
+module.exports = {encodeCeasar, decodeCeasar, encodeDecodeAtbash, encodeROT8, decodeROT8};
